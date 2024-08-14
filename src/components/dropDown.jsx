ece83630ee7c6d0908f234ad80a4bad76fd91fef
AnB School 0020 <anbschool0020@AnBs-Mac-mini-19.local>
2024-08-14 09:48:05 +0700

    Add: added syle to the dropdown component

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
 
 function DropDown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false)
@@ -14,17 +15,19 @@ function DropDown({ options, value, onChange }) {
             onChange(nextOption);
         }
     }
+    
+    const icon = !isOpen ? <GoChevronDown /> : <GoChevronUp />
 
     const renderOptions = options.map(option => {
         return (
-            <div key={ option.value } onClick={ () => handleOpen(option.label) }>{ option.label }</div>
+            <div className=" hover:bg-sky-100 rounded cursor-pointer p-1" key={ option.value } onClick={ () => handleOpen(option.label) }>{ option.label }</div>
         )
     })
 
     return (
-        <div>
-            <div onClick={ () => handleOpen() }>{value}</div>
-            { isOpen && <div>{ renderOptions }</div>}
+        <div className=" w-48 relative">
+            <div className=" flex justify-between items-center cursor-pointer border rounded shadow bg-white p-3 w-full" onClick={ () => handleOpen() }>{value} {icon}</div>
+            { isOpen && <div className=" absolute top-full border p-3 shadow bg-white w-full">{ renderOptions }</div>}
         </div>
     )
 }
